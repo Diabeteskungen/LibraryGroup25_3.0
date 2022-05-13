c3254a8c588485d72a0aae24618bf992024622b6
Davve <davhag@live.se>
2022-05-13 15:56:16 +0200

    Navigation till login fungerar delvis

--- src/main/java/com/example/librarygroup25/StartpageController.java
+++ src/main/java/com/example/librarygroup25/StartpageController.java
@@ -1,14 +1,29 @@
 package com.example.librarygroup25;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartpageController {
     @FXML
-    private Label welcomeText;
+    private Label stage;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLoginButtonClick(ActionEvent event) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+
+
+
     }
 }
+
