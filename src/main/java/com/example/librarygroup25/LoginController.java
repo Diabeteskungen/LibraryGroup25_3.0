8d6e11e5093fdbbc7566865f378e08af50612aa3
Davve <davhag@live.se>
2022-05-17 15:58:21 +0200

    början på login funktion

--- src/main/java/com/example/librarygroup25/LoginController.java
+++ src/main/java/com/example/librarygroup25/LoginController.java
@@ -1,4 +1,33 @@
 package com.example.librarygroup25;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.sql.*;
 
 public class LoginController {
+
+    @FXML
+    private TextField UsernameField;
+
+    private void login() throws SQLException {
+        Query query = new Query();
+        String findUser = "{ CALL spFindUser(?) }";
+        String checkPassword = "{ CALL spCheckPassword(?, ?)}";
+        String username = UsernameField.getText();
+        ResultSet resultSet;
+        String result = null;
+
+        try {
+            resultSet = query.querySingle(findUser, username);
+
+            while (resultSet.next()) {
+                result = resultSet.getString("result");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
+
