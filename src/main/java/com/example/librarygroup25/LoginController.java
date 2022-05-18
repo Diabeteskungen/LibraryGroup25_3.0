f4788356f3ccdbf454674dbd6385990b93fd1643
Davve <davhag@live.se>
2022-05-18 15:09:16 +0200

    Added error message for user login screen

--- src/main/java/com/example/librarygroup25/LoginController.java
+++ src/main/java/com/example/librarygroup25/LoginController.java
@@ -4,13 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.sql.*;
 
 public class LoginController {
-
 
     @FXML
     private TextField UsernameField;
@@ -19,28 +19,27 @@ public class LoginController {
     private TextField PasswordField;
 
     @FXML
+    private Label LoginErrorText;
+
+    @FXML
     protected void onRegisterClick(ActionEvent event) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("register.fxml"));
         Parent root1 = (Parent) fxmlLoader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(root1));
         stage.show();
-        stage.close();
+
     }
 
     @FXML
     protected void onLoginClick(ActionEvent event) throws Exception {
-        Connection con = ConnectionMYDB.getConnection();
-        ConnectionMYDB connection = new ConnectionMYDB();
         Query query = new Query();
         String checkPassword = ("{ CALL spCheckPassword(?, ?) }");
         String username = UsernameField.getText();
         String password = PasswordField.getText();
         ResultSet resultSet;
-        String result = null;
         String pwdFromDB = null;
-
-        //CallableStatement checkPassword = con.prepareCall("{call spCheckPassword(?, ?)}");
+        LoginErrorText.setText("Wrong username or password");
 
         try {
             resultSet = query.queryDouble(checkPassword, username, password);
@@ -56,11 +55,9 @@ public class LoginController {
                     Stage stage = new Stage();
                     stage.setScene(new Scene(root1));
                     stage.show();
-                } else {
-                    System.out.println("User or password is wrong");
-
-
                 }
+
+
             }
         }catch(SQLException e){
                 e.printStackTrace();
