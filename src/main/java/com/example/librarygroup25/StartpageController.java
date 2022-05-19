757117cc16acb9ba8669e1dc3c34df3a5ca54c67
diabeteskungen99 <lukasbergman@live.se>
2022-05-19 15:02:27 +0200

    Jocke is livlina

--- src/main/java/com/example/librarygroup25/StartpageController.java
+++ src/main/java/com/example/librarygroup25/StartpageController.java
@@ -1,20 +1,32 @@
 package com.example.librarygroup25;
 
+import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class StartpageController {
+public class StartpageController implements Initializable {
+
 
     @FXML
     private Label stage;
+
 
     @FXML
     private Button buttonLogin;
@@ -23,27 +35,99 @@ public class StartpageController {
     private Button buttonAdmin;
 
     @FXML
-    protected void onLoginButtonClick(ActionEvent event) throws Exception {
-        Stage stage = (Stage) buttonLogin.getScene().getWindow();
-        stage.close();
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("loginUser.fxml"));
-        Parent root1 = (Parent) fxmlLoader.load();
-        stage = new Stage();
-        stage.setScene(new Scene(root1));
-        stage.show();
+    private TextField searchInput;
 
-    }
     @FXML
-    public void onAdminButtonClick(ActionEvent event) throws IOException {
-        Stage stage = (Stage) buttonAdmin.getScene().getWindow();
-        stage.close();
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("loginAdmin.fxml"));
-        Parent root1 = (Parent) fxmlLoader.load();
-        stage = new Stage();
-        stage.setScene(new Scene(root1));
-        stage.show();
+    private TableView<Items> searchResult;
+
+    @FXML
+    private TableColumn<Items,String> title;
+
+    @FXML
+    private TableColumn<Items,String> author;
+
+    @FXML
+    private TableColumn<Items,String> genre;
+
+    @FXML
+    private TableColumn<Items,String> id;
+
+    public static ObservableList<Items> itemsinDB = FXCollections.observableArrayList();
+
+
+    protected void search () throws Exception {
+
+        Query query = new Query();
+        String checkBookExist = ("{ CALL spFindBook (?) }");
+        String checkDvdExist = ("{ CALL spFindDvd (?) }");
+        String searchInputText = searchInput.getText();
+        ResultSet resultSet;
+        String outputFromDB = null;
+
+
+        try {
+            resultSet = query.querySingle(checkBookExist, searchInputText);
+
+            while (resultSet.next()) {
+                itemsinDB.add(new Items(resultSet.getString("isbn"), resultSet.getString("bTitle"),resultSet.getString("author"),resultSet.getString("tags")));
+
+
+            }
+            resultSet = query.querySingle(checkDvdExist, searchInputText);
+
+            while (resultSet.next()) {
+                itemsinDB.add(new Items(resultSet.getString("artNr"), resultSet.getString("dTitle"),resultSet.getString("director"),resultSet.getString("tags")));
+
+
+
+            }
+            searchResult.setVisible(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+
+
+                public void onLoginButtonClick (ActionEvent event) throws Exception {
+                Stage stage = (Stage) buttonLogin.getScene().getWindow();
+                stage.close();
+                FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("loginUser.fxml"));
+                Parent root1 = (Parent) fxmlLoader.load();
+                stage = new Stage();
+                stage.setScene(new Scene(root1));
+                stage.show();
+
+            }
+
+            public void onAdminButtonClick (ActionEvent event) throws IOException {
+                Stage stage = (Stage) buttonAdmin.getScene().getWindow();
+                stage.close();
+                FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("loginAdmin.fxml"));
+                Parent root1 = (Parent) fxmlLoader.load();
+                stage = new Stage();
+                stage.setScene(new Scene(root1));
+                stage.show();
+            }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+            title.setCellValueFactory(new PropertyValueFactory<>("title"));
+            author.setCellValueFactory(new PropertyValueFactory<>("author"));
+            genre.setCellValueFactory(new PropertyValueFactory<>("genre"));
+            id.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+            searchResult.setItems(itemsinDB);
+
+
+        }
+
+    public void searchbuttonaction(ActionEvent actionEvent) throws Exception {
+        search();
     }
 }
-
 
 
