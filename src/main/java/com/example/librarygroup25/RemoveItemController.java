f793bf6c40c84e1ac93b37fd1ddb473695932801
Davve <davhag@live.se>
2022-05-20 15:37:36 +0200

    snopp

--- src/main/java/com/example/librarygroup25/RemoveItemController.java
+++ src/main/java/com/example/librarygroup25/RemoveItemController.java
@@ -2,11 +2,20 @@ package com.example.librarygroup25;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 import java.sql.*;
 
 public class RemoveItemController {
+
+    @FXML
+    private Button homeButton;
 
     @FXML
     private TextField isbnField;
@@ -16,6 +25,16 @@ public class RemoveItemController {
 
     @FXML
     private Label removeSuccessMessage;
+
+    public void onHomeButtonPress(ActionEvent event) throws Exception {
+        Stage stage = (Stage) homeButton.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("startpage.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
 
     public void onRemoveButtonPress(ActionEvent actionEvent) {
         Query query = new Query();
