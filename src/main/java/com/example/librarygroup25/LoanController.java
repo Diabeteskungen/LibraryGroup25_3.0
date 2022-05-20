3a55c09ee3a5919e9b6cb5f8268a261a1b206d17
Davve <davhag@live.se>
2022-05-20 20:20:47 +0200

    Loan boii

--- src/main/java/com/example/librarygroup25/LoanController.java
+++ src/main/java/com/example/librarygroup25/LoanController.java
@@ -3,6 +3,7 @@ package com.example.librarygroup25;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -28,8 +29,13 @@ public class LoanController {
     private VBox RecieptFrame;
 
     @FXML
-    private void onButtonCheckLoan(ActionEvent event) throws SQLException { //Primary function that calls SP:s from database and checks that the book that is trying to be returned fills requirements
+    private Button closeReceiptButton;
 
+    @FXML
+    private Label loanBookErrorText;
+
+    @FXML
+    private void onButtonCheckLoan(ActionEvent event) throws SQLException { //Primary function that calls SP:s from database and checks that the book that is trying to be returned fills requirements
         String checkloan = "{ CALL spRegisterloan (?, ?) }";
         Query query = new Query();
         String barcode = BarcodeField.getText();
@@ -56,11 +62,20 @@ public class LoanController {
             RecieptText.setText("A recipt has been sent to " + emailrecipt + " containing the following information:\n\nTitle: "+
                     titlerecipt + "\nBarcode: " + barcoderecipt + "\nBorrowed Date: " + borrowedDaterecipt + "\nLast Return Date: " + lastReturnDaterecipt);
             RecieptFrame.setVisible(true);
+            closeReceiptButton.setVisible(true);
 
 
         } catch (SQLException e) {
+            loanBookErrorText.setText("You cannot loan this at this time.");
             e.printStackTrace();
             e.getCause();
         }
+    }
+
+    public void onCLoseReceiptButton(ActionEvent event) {
+        RecieptText.setText("");
+        RecieptFrame.setVisible(false);
+        closeReceiptButton.setVisible(false);
+
     }
 }
