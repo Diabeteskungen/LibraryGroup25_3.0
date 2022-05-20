bc323d370d253abfe7975902a8950449db768f2d
Olof <oo.rojvall@gmail.com>
2022-05-20 14:48:03 +0200

    add item controller/fxml

--- src/main/java/com/example/librarygroup25/AddItemController.java
+++ src/main/java/com/example/librarygroup25/AddItemController.java
@@ -1,4 +1,65 @@
 package com.example.librarygroup25;
 
+import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
 public class AddItemController {
+
+    @FXML
+    private Button AddBookButton;
+
+    @FXML
+    private Button AddDvdButton;
+
+    @FXML
+    private Button AddCopyButton;
+
+
+    public void openAddBook (ActionEvent event) throws IOException {
+        Stage stage = (Stage) AddBookButton.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("AddBook.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void openAddDvd (ActionEvent event) throws IOException {
+        Stage stage = (Stage) AddDvdButton.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("AddDvd.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    public void openAddCopy (ActionEvent event) throws IOException {
+        Stage stage = (Stage) AddCopyButton.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("AddCopy.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
 }
